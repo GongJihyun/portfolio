7dc77edec2d81f9c3cd53230808d78907ebfa439
Jihyun_project <rhdwlgus215@gmail.com>
2025-04-28 11:11:25 +0900

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -230,9 +230,9 @@ jQuery(function ($) {
     // -------------------------------------------------------------
     // Vidio auto play
     // -------------------------------------------------------------
-    (function () {
+    /*(function () {
 
-        /* Vimeo API: http://developer.vimeo.com/player/js-api */
+         Vimeo API: http://developer.vimeo.com/player/js-api 
 
         var iframe = document.getElementById('nofocusvideo');
         // $f == Froogaloop
@@ -245,7 +245,7 @@ jQuery(function ($) {
         $('.modal').on('shown.bs.modal', function () {
             player.api('play');
         })
-    }());
+    }());*/
 
 
 
